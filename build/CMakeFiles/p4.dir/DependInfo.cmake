453cc2d59b812172d04b1eead185528eba17a24f
sannd22 <davidsann92@gmail.com>
2019-11-08 14:46:40 -0800

    Add files via upload

---
+++ build/CMakeFiles/p4.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/davidsann/Desktop/471/P4/ext/glad/src/glad.c" "/Users/davidsann/Desktop/471/P4/build/CMakeFiles/p4.dir/ext/glad/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../ext"
+  "../ext/glad/include"
+  "/usr/local/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/davidsann/Desktop/471/P4/ext/tiny_obj_loader/tiny_obj_loader.cpp" "/Users/davidsann/Desktop/471/P4/build/CMakeFiles/p4.dir/ext/tiny_obj_loader/tiny_obj_loader.cpp.o"
+  "/Users/davidsann/Desktop/471/P4/src/GLSL.cpp" "/Users/davidsann/Desktop/471/P4/build/CMakeFiles/p4.dir/src/GLSL.cpp.o"
+  "/Users/davidsann/Desktop/471/P4/src/MatrixStack.cpp" "/Users/davidsann/Desktop/471/P4/build/CMakeFiles/p4.dir/src/MatrixStack.cpp.o"
+  "/Users/davidsann/Desktop/471/P4/src/Program.cpp" "/Users/davidsann/Desktop/471/P4/build/CMakeFiles/p4.dir/src/Program.cpp.o"
+  "/Users/davidsann/Desktop/471/P4/src/Shape.cpp" "/Users/davidsann/Desktop/471/P4/build/CMakeFiles/p4.dir/src/Shape.cpp.o"
+  "/Users/davidsann/Desktop/471/P4/src/Texture.cpp" "/Users/davidsann/Desktop/471/P4/build/CMakeFiles/p4.dir/src/Texture.cpp.o"
+  "/Users/davidsann/Desktop/471/P4/src/WindowManager.cpp" "/Users/davidsann/Desktop/471/P4/build/CMakeFiles/p4.dir/src/WindowManager.cpp.o"
+  "/Users/davidsann/Desktop/471/P4/src/main.cpp" "/Users/davidsann/Desktop/471/P4/build/CMakeFiles/p4.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ext"
+  "../ext/glad/include"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/p4.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p4.dir/ext/glad/src/glad.c.o"
+  "CMakeFiles/p4.dir/ext/tiny_obj_loader/tiny_obj_loader.cpp.o"
+  "CMakeFiles/p4.dir/src/GLSL.cpp.o"
+  "CMakeFiles/p4.dir/src/MatrixStack.cpp.o"
+  "CMakeFiles/p4.dir/src/Program.cpp.o"
+  "CMakeFiles/p4.dir/src/Shape.cpp.o"
+  "CMakeFiles/p4.dir/src/Texture.cpp.o"
+  "CMakeFiles/p4.dir/src/WindowManager.cpp.o"
+  "CMakeFiles/p4.dir/src/main.cpp.o"
+  "p4.pdb"
+  "p4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/p4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
